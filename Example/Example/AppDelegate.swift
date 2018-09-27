9457f6bf3fd2939fc53b4c017e766c920d0cd28b
yu zhou <yu.zhou@razer.com>
2018-09-27 18:37:12 +0800

    增加workSpace

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  ZYNavigationBar
+//  Example
 //
-//  Created by yu zhou on 11/09/2018.
+//  Created by yu zhou on 27/09/2018.
 //  Copyright © 2018 yu zhou. All rights reserved.
 //
 
@@ -12,6 +12,7 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
---
+++ ZYNavigationBar/ZYNavigationBar/ZYNavigationBar.h
@@ -0,0 +1,19 @@
+//
+//  ZYNavigationBar.h
+//  ZYNavigationBar
+//
+//  Created by yu zhou on 27/09/2018.
+//  Copyright © 2018 yu zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZYNavigationBar.
+FOUNDATION_EXPORT double ZYNavigationBarVersionNumber;
+
+//! Project version string for ZYNavigationBar.
+FOUNDATION_EXPORT const unsigned char ZYNavigationBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZYNavigationBar/PublicHeader.h>
+
+
